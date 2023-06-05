16ca3aff7cd097efd019b0afc0695eead2ad5aed
Harishkumar Yetam <119168445+harrypappi@users.noreply.github.com>
2023-06-05 15:35:49 +0530

    first

---
+++ Program1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Employee
+{
+    private bool isPresent;
+
+    public void MarkAttendance()
+    {
+        Random random = new Random();
+        isPresent = random.Next(0, 2) == 1;
+    }
+
+    public string GetAttendanceStatus()
+    {
+        return isPresent ? "Present" : "Absent";
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Employee employee = new Employee();
+        employee.MarkAttendance();
+
+        string attendanceStatus = employee.GetAttendanceStatus();
+        Console.WriteLine("Employee is " + attendanceStatus);
+    }
+}
