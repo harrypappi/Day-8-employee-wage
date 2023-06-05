03c14a04fdbb48ff61fc3d46a49c9fe9322719c8
Harishkumar Yetam <119168445+harrypappi@users.noreply.github.com>
2023-06-05 15:36:41 +0530

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Employee
+{
+    // Class variables
+    private static double baseHourlyRate = 10.0;
+    private static double overtimeRate = 1.5;
+
+    // Class method to compute employee wage
+    public static double ComputeWage(double hoursWorked)
+    {
+        double wage = 0.0;
+
+        if (hoursWorked <= 40)
+        {
+            wage = hoursWorked * baseHourlyRate;
+        }
+        else
+        {
+            double overtimeHours = hoursWorked - 40;
+            wage = (40 * baseHourlyRate) + (overtimeHours * baseHourlyRate * overtimeRate);
+        }
+
+        return wage;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double hoursWorked = 45;
+        double wage = Employee.ComputeWage(hoursWorked);
+
+        Console.WriteLine($"Employee wage for {hoursWorked} hours: ${wage}");
+    }
+}
